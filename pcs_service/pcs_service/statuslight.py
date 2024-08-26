cafd1b11405d01e4aa825b4fd095605fd07d934f
Ben Worth <benjamin.worth@canberra.edu.au>
2024-08-26 20:22:31 +1000

    statuslight working

--- pcs_service/pcs_service/statuslight.py
+++ pcs_service/pcs_service/statuslight.py
@@ -15,14 +15,14 @@ class StatusLight(Node):
             '/status',
             10)
         timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.timer = self.create_timer(timer_period, self.publish_status)
         # create a set for storing currently awaited jobs
         self.jobs_todo = set()
         # make a subscriber to /capture
         self.capture_subscription = self.create_subscription(
             CaptureRequest,
             '/capture',
-            self.listener_callback,
+            self.capture_request_callback,
             10)
         self.capture_subscription
         # make a publisher for forwarding requests to capture action node
@@ -30,27 +30,44 @@ class StatusLight(Node):
             CaptureRequest,
             '/jobs_in',
             10)
+        self.jobs_done_subscription = self.create_subscription(
+            CaptureRequest,
+            '/jobs_done',
+            self.job_done_callback,
+            10)
 
-    def timer_callback(self):
-        # publish true if there are no jobs todo
+    def publish_status(self):
+        # publish ready true if there are no jobs todo
         msg = Bool()
-        if (len(self.jobs_todo) == 0):
-            msg.data = True
-        else:
+        has_jobs = (len(self.jobs_todo) != 0) # its either true or false
+        if has_jobs:
             msg.data = False
+            self.get_logger().info(f'Current jobs: {self.jobs_todo}',
+            throttle_duration_sec=10)
+        else: # no jobs duh
+            msg.data = True
         self.status_publisher.publish(msg)
-        self.get_logger().info(f'Publishing Ready: {msg.data}', throttle_duration_sec=3)
+        self.get_logger().info(f'Publishing Ready: {msg.data}',
+            throttle_duration_sec=3)
 
-    def listener_callback(self, msg):
+    def capture_request_callback(self, msg):
         # wait for capture requests and add them to the jobs todo list
         # also publish the new job to /jobs_in
         # msg will be of type 'CaptureRequest', {label1:String, label2:String}
-        self.get_logger().info(f'Adding new job: {msg.label1} {msg.label2}')
-        # add the job to the local set of todos
+        self.get_logger().info(f'New capture request: {msg.label1} {msg.label2}')
+        # add the job to the local set of todos (as a tuple)
         self.jobs_todo.add((msg.label1, msg.label2))
         # forward the message to /jobs_in
+        self.jobs_in_publisher.publish(msg)
 
-
+    def job_done_callback(self, msg):
+        # wait for messages on /jobs_done then remove them from the local todos
+        # this should free up the status to ready true after all jobs are done
+        try:
+            self.jobs_todo.remove((msg.label1, msg.label2))
+            self.get_logger().info(f'Done job: {msg.label1} {msg.label2}')
+        except KeyError:
+            self.get_logger().error(f'Unknown done job {msg.label1} {msg.label2}')
 
 
 def main(args=None):
